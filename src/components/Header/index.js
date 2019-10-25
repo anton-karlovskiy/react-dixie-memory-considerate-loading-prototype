bc89ebe9d1e2cd38684d0b42100128197036bf9a
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-10-25 22:38:34 +0300

    cleaned up unused css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ import logo from '../../assets/images/logo.webp';
 const Header = () => {
   return (
     <div className='header'>
-      <div className='bottom-part bottom-left'>
+      <div className='bottom-left'>
         <nav className='header-nav'>
           <a href='/' className='header-nav-item'>Home</a>
           <a href='https://dixiemech.store/' className='header-nav-item'>Shop</a>
@@ -16,12 +16,12 @@ const Header = () => {
           <a href='https://dixiemech.store/pages/faq' className='header-nav-item'>Help</a>
         </nav>
       </div>
-      <div className='bottom-part bottom-center'>
+      <div className='bottom-center'>
         <a href='/' className='header-branding'>
           <img src={logo} alt='Dixie Mech' className='header-branding-logo' />
         </a>
       </div>
-      <div className='bottom-part bottom-right'>
+      <div className='bottom-right'>
         <nav className='header-nav'>
           <a href='https://discordapp.com/invite/KaZjphH' target='_blank' rel='noopener noreferrer' className='header-nav-item'>Discord</a>
           <a href='https://dixiemech.store/account/login' className='header-nav-item'>Account</a>
