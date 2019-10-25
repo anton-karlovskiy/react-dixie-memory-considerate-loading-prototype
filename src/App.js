140bf1eba434e0f386573b8c2a5c3af0eb0fb05a
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-10-25 13:29:25 +0300

    updated background styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 
 import Header from './components/Header';
+import MainContent from './components/MainContent';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <MainContent />
     </div>
   );
 }
---
+++ src/components/MainContent/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import './main-content.css';
+
+const MainContent = () => {
+  return (
+    <div className='main-content'>MainContent</div>
+  );
+};
+
+export default MainContent;
