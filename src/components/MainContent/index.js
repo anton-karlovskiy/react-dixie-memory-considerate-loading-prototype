ab43f1e5341fb6d2bed9ce4b07a89954026dbe7e
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-10-28 19:43:35 +0300

    removed code snippet part as instructed

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -8,13 +8,6 @@ const MainContent = () => {
   return (
     <div className='main-content'>
       <ThreeBox />
-      <div className='code-snippet'>
-        <div className='block'>
-          <p>> Starting 'deploy' ...</p>
-          <p>> Uploading: groupbuys/keysets/gmk-dracula/master.tar.gz</p>
-          <p>> [========================================] 100% 10s</p>
-        </div>
-      </div>
     </div>
   );
 };
